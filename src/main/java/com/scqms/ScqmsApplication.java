3b9a5b97e63f903115493bd4b0c173d6cd51842c
Guru <guru85111@gmail.com>
2025-10-30 20:17:02 +0530

    initial commit

---
+++ src/main/java/com/scqms/ScqmsApplication.java
@@ -0,0 +1,14 @@
+package com.scqms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScqmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScqmsApplication.class, args);
+		System.out.println("Hello");
+	}
+
+}
